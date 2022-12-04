c6a12b511973fc7f4ca362dc76762ea0598768fb
Nazar <nazarmasukov@gmail.com>
2022-12-04 19:57:45 +0300

    Implemented Dataflow pipeline(Console app).

---
+++ ConsoleApp/DataflowPipeline.cs
@@ -0,0 +1,84 @@
+using System.Threading.Tasks.Dataflow;
+using Core;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+public class DataflowPipeline
+{
+    private int _maxCuncurrentInput;
+    private int _maxCuncurrentOutput;
+    private int _maxConcurrentProcessing;
+    private string _input;
+    private string _output;
+
+    public DataflowPipeline(string input, string output, int maxCuncurrentInput, int maxCuncurrentOutput, int maxConcurrentProcessing)
+    {
+        _input = input;
+        _output = output;
+        _maxCuncurrentInput = maxCuncurrentInput;
+        _maxCuncurrentOutput = maxCuncurrentOutput;
+        _maxConcurrentProcessing = maxConcurrentProcessing;
+    }
+
+    public async Task GenerateTestsAsync()
+    {
+        var bufferBlock = new BufferBlock<string>();
+
+        var readingBlock = new TransformBlock<string, string>(
+            async path => await GetSourceCode(path),
+            new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _maxCuncurrentInput });
+
+        var processingBlock = new TransformManyBlock<string, string>(
+            content => ProcessSourceCode(content),
+            new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _maxConcurrentProcessing });
+
+        var writingBlock = new ActionBlock<string>(async tests => await WriteTests(tests),
+            new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _maxCuncurrentOutput });
+
+        var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+        bufferBlock.LinkTo(readingBlock, linkOptions);
+        readingBlock.LinkTo(processingBlock, linkOptions);
+        processingBlock.LinkTo(writingBlock, linkOptions);
+
+        foreach (var file in Directory.EnumerateFiles(_input))
+        {
+            bufferBlock.Post(file);
+        }
+
+        bufferBlock.Complete();
+
+        await writingBlock.Completion;
+    }
+
+    private async Task<string> GetSourceCode(string filePath)
+    {
+        string sourceCode;
+        using (var streamReader = new StreamReader(filePath))
+        {
+            sourceCode = await streamReader.ReadToEndAsync();
+        }
+        return sourceCode;
+    }
+
+    private List<string> ProcessSourceCode(string sourceCode)
+    {
+        var testGenerator = new XUnitTestGenerator();
+
+        var tests = testGenerator.GenerateTestsAsync(sourceCode);
+
+        return tests;
+    }
+
+    private async Task WriteTests(string tests)
+    {
+        var fileName = CSharpSyntaxTree.ParseText(tests).GetRoot()
+            .DescendantNodes().OfType<ClassDeclarationSyntax>().First().Identifier.Text;
+
+        using (var streamWriter = new StreamWriter($"{_output}\\{fileName}.cs"))
+        {
+            await streamWriter.WriteAsync(tests);
+        }
+
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks.Dataflow;
+
+// Demonstrates how to create a basic dataflow pipeline.
+// This program downloads the book "The Iliad of Homer" by Homer from the Web
+// and finds all reversed words that appear in that book.
+static class Program
+{
+    static async void Main()
+    {
+        string inputDir;
+        inputDir = Console.ReadLine();
+        while (!Directory.Exists(inputDir)){
+            Console.WriteLine($"Directory doesn't exist: \"{inputDir}\".");
+            inputDir = Console.ReadLine();
+        }        
+        Console.WriteLine($"Input directory: \"{inputDir}\".");
+
+        string outputDir;
+        outputDir = Console.ReadLine();
+        while (!Directory.Exists(outputDir)){
+            Console.WriteLine($"Directory doesn't exist: \"{outputDir}\".");
+            outputDir = Console.ReadLine();
+        }    
+        Console.WriteLine($"Output directory: \"{outputDir}\".");  
+        
+        var dataFlowPipeline = new DataflowPipeline(inputDir, outputDir, 3, 3, 3);
+        await dataFlowPipeline.GenerateTestsAsync();        
+    }
+}
